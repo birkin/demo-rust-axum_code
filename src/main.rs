e5d738949486c3ba79a9f2958b4657c387057126
Birkin James Diana <birkin.diana@gmail.com>
2022-12-13 08:15:27 -0500

    implements fallback route-handler.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
-// fn main() {
-//     println!("Hello, world!");
-// }
 
-use axum::Router;
-use axum::routing::get;
+use axum::{routing::get, Router};
 
 
 #[tokio::main]
 pub async fn main() {
-    // Build our application by creating our router.
-    let app = Router::new()
-        // .route( "/", axum::routing::get( || async {"Hello, World!"} ) 
-        .route( "/", get( hello )
+    // Build our application by creating our router
+    let app: Router = axum::Router::new()
+        // .fallback( fallback.into_service() )
+        .fallback( fallback )  // different from tutorial; whew
+        .route( "/", get( hello )  
     );
 
-    // Run our application as a hyper server on http://localhost:3000.
+    // Run our application as a hyper server on http://localhost:3000
     axum::Server::bind( &"0.0.0.0:3000".parse().unwrap())
         .serve( app.into_make_service() )
         .await
         .unwrap();
 }
 
+// route handlers ---------------------------------------------------
+
+pub async fn fallback( uri: axum::http::Uri ) -> impl axum::response::IntoResponse {
+    ( axum::http::StatusCode::NOT_FOUND, format!("No route {}", uri) )
+}
 
 pub async fn hello() -> String {
     "Hello, World!".into()

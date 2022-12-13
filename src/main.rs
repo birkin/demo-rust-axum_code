9565780a82059ebd98ea9b8cd14b74e1c12b29aa
Birkin James Diana <birkin.diana@gmail.com>
2022-12-13 07:25:44 -0500

    refactors route to be handled by a function.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@
 // }
 
 use axum::Router;
+use axum::routing::get;
 
 
 #[tokio::main]
 pub async fn main() {
     // Build our application by creating our router.
     let app = Router::new()
-        .route( "/", axum::routing::get( || async {"Hello, World!"} ) 
+        // .route( "/", axum::routing::get( || async {"Hello, World!"} ) 
+        .route( "/", get( hello )
     );
 
     // Run our application as a hyper server on http://localhost:3000.
@@ -17,4 +19,9 @@ pub async fn main() {
         .serve( app.into_make_service() )
         .await
         .unwrap();
+}
+
+
+pub async fn hello() -> String {
+    "Hello, World!".into()
 }

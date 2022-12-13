6eec94a6b55136463be0e87a40477139f78eb694
Birkin James Diana <birkin.diana@gmail.com>
2022-12-13 07:20:18 -0500

    implements initial route handling.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+use axum::Router;
+
+
+#[tokio::main]
+pub async fn main() {
+    // Build our application by creating our router.
+    let app = Router::new()
+        .route( "/", axum::routing::get( || async {"Hello, World!"} ) 
+    );
+
+    // Run our application as a hyper server on http://localhost:3000.
+    axum::Server::bind( &"0.0.0.0:3000".parse().unwrap())
+        .serve( app.into_make_service() )
+        .await
+        .unwrap();
 }

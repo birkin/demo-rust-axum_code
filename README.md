9d7b52c900fbf3d813f3f86b8fc45e8452ea4512
Birkin James Diana <birkin.diana@gmail.com>
2022-12-24 10:46:41 -0500

    implements graceful shutdown.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 Going through <https://github.com/joelparkerhenderson/demo-rust-axum>.
+
+Currently at:
+<https://github.com/joelparkerhenderson/demo-rust-axum#create-axum-routes-and-axum-handlers>
+
+Done...
+
+- Hello, World!
+
+- Hello, World! with a handler
+
+- Create a router fallback
+
+- Graceful shutdown
+
+=
--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,17 @@ pub async fn main() {
     // Run our application as a hyper server on http://localhost:3000
     axum::Server::bind( &"0.0.0.0:3000".parse().unwrap())
         .serve( app.into_make_service() )
+        .with_graceful_shutdown( shutdown_signal() )
         .await
         .unwrap();
-}
+
+    // // Run our application as a hyper server on http://localhost:3000
+    // axum::Server::bind( &"0.0.0.0:3000".parse().unwrap())
+    //     .serve( app.into_make_service() )
+    //     .await
+    //     .unwrap();
+
+} // end main()
 
 // route handlers ---------------------------------------------------
 
@@ -27,3 +35,12 @@ pub async fn fallback( uri: axum::http::Uri ) -> impl axum::response::IntoRespon
 pub async fn hello() -> String {
     "Hello, World!".into()
 }
+
+// graceful shutdown ------------------------------------------------
+
+async fn shutdown_signal() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect( "expect tokio signal ctrl-c" );
+    println!( "Received Ctrl-C" );
+}   

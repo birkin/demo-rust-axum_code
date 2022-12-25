3e1caf366cbf3ed583f969270c9445f86121f234
Birkin James Diana <birkin.diana@gmail.com>
2022-12-25 11:36:56 -0500

    adds sub-dir html url and status url.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,38 @@
-
-use axum::{
-    routing::get, Router,
-    response::Html,
-};
-
+use axum::{http::StatusCode, response::Html, routing::get, Router};
 
 #[tokio::main]
 pub async fn main() {
-    // Build our application by creating our router
+    // router -------------------------------------------------------
     let app: Router = axum::Router::new()
         // .fallback( fallback.into_service() )
-        .fallback( fallback )  // different from tutorial; whew
-        .route( "/", get(hello) )
-        .route( "/demo_from_string.html", get(get_demo_html_from_string) )
-        .route( "/demo_from_sibling_file.html", get(get_demo_html_from_sibling_file) )
-    ;
+        .fallback(fallback) // different from tutorial; whew
+        .route("/", get(hello))
+        .route("/demo_from_string.html", get(get_demo_html_from_string))
+        .route(
+            "/demo_from_sibling_file.html",
+            get(get_demo_html_from_sibling_file),
+        )
+        .route(
+            "/demo_from_html_sub_dir.html",
+            get(get_demo_html_from_sub_dir),
+        )
+        .route("/demo_status_code", get(get_demo_status_code));
 
-    // Run our application as a hyper server on http://localhost:3000
-    axum::Server::bind( &"0.0.0.0:3000".parse().unwrap())
-        .serve( app.into_make_service() )
-        .with_graceful_shutdown( shutdown_signal() )
+    // run app as hyper server on http://localhost:3000 -------------
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
-
-    // // Run our application as a hyper server on http://localhost:3000
-    // axum::Server::bind( &"0.0.0.0:3000".parse().unwrap())
-    //     .serve( app.into_make_service() )
-    //     .await
-    //     .unwrap();
-
 } // end main()
 
 // route handlers ---------------------------------------------------
 
-pub async fn fallback( uri: axum::http::Uri ) -> impl axum::response::IntoResponse {
-    ( axum::http::StatusCode::NOT_FOUND, format!("No route {}", uri) )
+pub async fn fallback(uri: axum::http::Uri) -> impl axum::response::IntoResponse {
+    (
+        axum::http::StatusCode::NOT_FOUND,
+        format!("No route {}", uri),
+    )
 }
 
 pub async fn hello() -> String {
@@ -46,7 +44,15 @@ pub async fn get_demo_html_from_string() -> Html<&'static str> {
 }
 
 pub async fn get_demo_html_from_sibling_file() -> Html<&'static str> {
-    include_str!( "hello_from_sibling_file.html" ).into()
+    include_str!("./hello_from_sibling_file.html").into()
+}
+
+pub async fn get_demo_html_from_sub_dir() -> Html<&'static str> {
+    include_str!("./html/hello_from_sub_dir.html").into()
+}
+
+pub async fn get_demo_status_code() -> (StatusCode, String) {
+    (axum::http::StatusCode::OK, "200/ OK".to_string())
 }
 
 // graceful shutdown ------------------------------------------------
@@ -54,6 +60,6 @@ pub async fn get_demo_html_from_sibling_file() -> Html<&'static str> {
 async fn shutdown_signal() {
     tokio::signal::ctrl_c()
         .await
-        .expect( "expect tokio signal ctrl-c" );
-    println!( "Received Ctrl-C" );
-}   
+        .expect("expect tokio signal ctrl-c");
+    println!("Received Ctrl-C");
+}
